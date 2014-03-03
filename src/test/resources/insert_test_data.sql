c3d26466a6265096c3b51ee5c0fc679234eb81a6
David Shorthouse <davidpshorthouse@gmail.com>
2014-03-03 14:42:16 -0500

    Enhance test data

--- src/test/resources/insert_test_data.sql
+++ src/test/resources/insert_test_data.sql
@@ -1,29 +1,29 @@
 INSERT INTO occurrence(
-auto_id,datasetname,sourcefileid,dwcaid,associatedmedia,scientificname,taxonrank,country,stateprovince,collectioncode,catalognumber,minimumelevationinmeters,maximumelevationinmeters)
+auto_id,datasetname,sourcefileid,dwcaid,associatedmedia,scientificname,taxonrank,country,stateprovince,collectioncode,catalognumber,minimumelevationinmeters,maximumelevationinmeters,syear,habitat)
 VALUES (
 1,'E. C. Smith Herbarium (ACAD)','acad-specimens',
 'ACAD-1','http://procyon.acadiau.ca/ecsmith/cgi-bin/image.cgi?ECS019597,jpeg; http://procyon.acadiau.ca/ecsmith/cgi-bin/image.cgi?ECS019597.jpeg',
-'Acer pseudoplatanus','species','Canada','Nova Scotia','ACAD','ECS019597',100,200
+'Acer pseudoplatanus','species','Canada','Nova Scotia','ACAD','ECS019597',100,200,1980,'Under rocks'
 );
 INSERT INTO occurrence_raw(
-auto_id,datasetname,sourcefileid,dwcaid,associatedmedia,scientificname,taxonrank,country,stateprovince,collectioncode,catalognumber)
+auto_id,datasetname,sourcefileid,dwcaid,associatedmedia,scientificname,taxonrank,country,stateprovince,collectioncode,catalognumber,year,habitat)
 VALUES (
 1,'E. C. Smith Herbarium (ACAD)','acad-specimens',
 'ACAD-1','http://procyon.acadiau.ca/ecsmith/cgi-bin/image.cgi?ECS019597,jpeg; http://procyon.acadiau.ca/ecsmith/cgi-bin/image.cgi?ECS019597.jpeg',
-'Acer pseudoplatanus','species','Canada','Nova Scotia','ACAD','ECS019597'
+'Acer pseudoplatanus','species','Canada','Nova Scotia','ACAD','ECS019597',1980,'Under rocks'
 );
 
 INSERT INTO occurrence(
-auto_id,datasetname,sourcefileid,dwcaid,scientificname,taxonrank,country,stateprovince,collectioncode,catalognumber)
+auto_id,datasetname,sourcefileid,dwcaid,scientificname,taxonrank,country,stateprovince,collectioncode,catalognumber,syear,habitat)
 VALUES (
 2,'E. C. Smith Herbarium (ACAD)','acad-specimens',
-'ACAD-2','Myosotis arvensis','species','Canada','Nova Scotia','ACAD','10440'
+'ACAD-2','Myosotis arvensis','species','Canada','Nova Scotia','ACAD','10440',2013,'On a lobster claw'
 );
 INSERT INTO occurrence_raw(
-auto_id,datasetname,sourcefileid,dwcaid,scientificname,taxonrank,country,stateprovince,collectioncode,catalognumber)
+auto_id,datasetname,sourcefileid,dwcaid,scientificname,taxonrank,country,stateprovince,collectioncode,catalognumber,year,habitat)
 VALUES (
 2,'E. C. Smith Herbarium (ACAD)','acad-specimens',
-'ACAD-2','Myosotis arvensis','species','Canada','Nova Scotia','ACAD','10440'
+'ACAD-2','Myosotis arvensis','species','Canada','Nova Scotia','ACAD','10440',2013, 'On a lobster claw'
 );
 
 
