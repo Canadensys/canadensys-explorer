3c188079effdd924ce9ab60b4f4cf9c5b9374a2b
Christian Gendreau <christiangendreau@gmail.com>
2014-03-17 14:53:46 -0400

    fixed 404 rendering and sitemesh3

--- src/main/java/net/canadensys/dataportal/occurrence/controller/HttpErrorController.java
+++ src/main/java/net/canadensys/dataportal/occurrence/controller/HttpErrorController.java
@@ -8,27 +8,32 @@ import net.canadensys.dataportal.occurrence.config.OccurrencePortalConfig;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.NoHandlerFoundException;
 
 /**
- * Controller to handle HTTP error (like 404) in a template.
+ * ControllerAdvice to handle exceptions allow usage of template/decorator.
  * @author canadensys
  *
  */
-@Controller
+@ControllerAdvice
 public class HttpErrorController {
 	
 	@Autowired
 	@Qualifier("occurrencePortalConfig")
 	private OccurrencePortalConfig appConfig;
 	
-	@RequestMapping(value="/errors/404.html")
-    public ModelAndView handle404(HttpServletRequest request) {
+	@ExceptionHandler(NoHandlerFoundException.class)
+	@ResponseStatus(value=HttpStatus.NOT_FOUND)
+	public ModelAndView handleNotFound(HttpServletRequest req){
 		HashMap<String,Object> modelRoot = new HashMap<String,Object>();
 		//Set common stuff (GoogleAnalytics, language, ...)
 		ControllerHelper.setPageHeaderVariables(appConfig, modelRoot);
         return new ModelAndView("error/404","root",modelRoot);
-    }
+	}
+	
 }
